aacd60a9c7823f19589e519e9c59741646dc8c7e
Armando del Río <adrio.coder@gmail.com>
2021-11-05 15:10:32 -0600

    refactor contracts, add test

--- assembly/__tests__/main.spec.ts
+++ assembly/__tests__/main.spec.ts
@@ -1,9 +1,25 @@
-import { setGreeting } from '..'
-import { storage, Context } from 'near-sdk-as'
+import { getVotes, userDidParticipate, incrementVotes, recordUser } from '..'
 
-describe('Greeting ', () => {
-  it('should be set and read', () => {
-    setGreeting('hello world')
-    storage.get<string>(Context.sender)
+describe('votescontract', () => {
+  it('should return 0 if no one has vote for a given candidate', () => {
+    expect(getVotes('planillaVerde')).toBe(0, 'counter should be 0 if no one as voted yet.')
+  })
+
+  it('should increment a votes count by one for a given candidate', () => {
+    const currenVotes = getVotes('planillaVerde')
+    incrementVotes('planillaVerde')
+    expect(getVotes('planillaVerde')).toBe(
+      currenVotes + 1,
+      'counter should increment by one after a incrementVotes gets called on a given voter.'
+    )
+  })
+
+  it('userDidParticipate should return false if he has not voted yed', () => {
+    expect(userDidParticipate('juan')).toBe(false)
+  })
+
+  it('userDidParticipate should return true if he is recorded as voter', () => {
+    recordUser('juan')
+    expect(userDidParticipate('juan')).toBe(true)
   })
 })
